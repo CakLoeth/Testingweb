b72e3a306ab7a05ec2056b0f17aa55e6a0eeb041
cakloeth <58018459+CakLoeth@users.noreply.github.com>
2025-09-20 21:51:04 +0700

    Delete ajax directory

--- ajax/js/laporan.js
+++
@@ -1,7 +0,0 @@
-$(".lapor").click(function () {
-    var jenis = $('#jenis-laporan').val();
-    var tanggalAwal = $('#awal').val();
-    var tanggalAkhir = $('#akhir').val();
-    var username = $('#username').val();
-    $(".tampil").load("ajax/tampilLaporan?jenis=" + jenis + "&awal=" + tanggalAwal + "&akhir=" + tanggalAkhir + "&username=" + username);
-});
